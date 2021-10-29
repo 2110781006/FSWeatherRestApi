20d29af6180e904af6b731c4be48d685b69dc90c
Michael Schmit <m.schmit@pke.at>
2021-10-29 16:56:40 +0200

    zwischencheckin

--- src/main/java/org/openapitools/connectors/WeatherstackConnector.java
+++ src/main/java/org/openapitools/connectors/WeatherstackConnector.java
@@ -10,8 +10,6 @@ import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Paths;
-import java.time.Duration;
 import java.util.Map;
 
 /**
